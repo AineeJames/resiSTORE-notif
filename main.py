da556e311648efba13dbf38d2bb33a25692fe9ca
AineeJames <olsenaiden33@gmail.com>
2023-02-07 14:02:30 -0800

    good time

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ pst = pytz.timezone('America/Los_Angeles')
 open_msg = "The resistore has opened!"
 closed_msg = "The resistore has closed..."
 
-prev_result = 1
+prev_result = 0
 while True:
     response = request.urlopen("https://resi.store/lux.bool").read()
     result = int(response.decode()[0])
     if result != prev_result:
-        pst_time = datetime.now(pst).strftime("%H:%M")
+        pst_time = datetime.now(pst).strftime("%I:%M %p")
         tag = "green_square,grinning" if result else "red_square,frowning_face"
         curl_cmd = f"""curl -d \"{open_msg if result else closed_msg}\" \
                             -H 'Title: ResiSTORE Status @ {pst_time}' \
                             -H 'Tags: {tag}' \
-                            ntfy.sh/resistore
+                            ntfy.sh/resistore_test
                     """
         os.system(curl_cmd)
     prev_result = result
