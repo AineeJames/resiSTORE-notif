79dae57eca8a35e66fabf33487c8afda6b5d47d7
AineeJames <olsenaiden33@gmail.com>
2023-02-07 11:47:00 -0800

    added time to title

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from urllib import request, parse
 from time import sleep
 import os
+import pytz
+from datetime import datetime
+
+pst = pytz.timezone('America/Los_Angeles')
 
 open_msg = "The resistore has opened!"
 closed_msg = "The resistore has closed..."
@@ -10,9 +14,10 @@ while True:
     response = request.urlopen("https://resi.store/lux.bool").read()
     result = int(response.decode()[0])
     if result != prev_result:
+        pst_time = datetime.now(pst).strftime("%H:%M")
         tag = "green_square,grinning" if result else "red_square,frowning_face"
         curl_cmd = f"""curl -d \"{open_msg if result else closed_msg}\" \
-                            -H 'Title: ResiSTORE Status' \
+                            -H 'Title: ResiSTORE Status @ {pst_time}' \
                             -H 'Tags: {tag}' \
                             ntfy.sh/resistore
                     """
