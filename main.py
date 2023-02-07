5150dae307f2d8110dfe54f2997b900f434ea1d0
James Wilcock <jameswilcock8@gmail.com>
2023-02-07 14:16:42 -0800

    added query function and initial value check

--- main.py
+++ main.py
@@ -4,24 +4,32 @@ import os
 import pytz
 from datetime import datetime
 
+def query_status() -> int:
+    response = request.urlopen("https://resi.store/lux.bool").read()
+    result = int(response.decode()[0])
+    return result
+
 pst = pytz.timezone('America/Los_Angeles')
 
 open_msg = "The resistore has opened!"
 closed_msg = "The resistore has closed..."
 
 print("Starting loop")
-prev_result = 1
-
+prev_result = query_status()
+init_status = "Open" if prev_result else "Closed"
+curl_cmd = f"""curl -d \"Starting status script, current status is {init_status}\" \
+                    ntfy.sh/resistore
+            """
+os.system(curl_cmd)
 while True:
-    response = request.urlopen("https://resi.store/lux.bool").read()
-    result = int(response.decode()[0])
+    result = query_status()
     if result != prev_result:
         pst_time = datetime.now(pst).strftime("%I:%M %p")
         tag = "green_square,grinning" if result else "red_square,frowning_face"
         curl_cmd = f"""curl -d \"{open_msg if result else closed_msg}\" \
                             -H 'Title: ResiSTORE Status @ {pst_time}' \
                             -H 'Tags: {tag}' \
-                            ntfy.sh/resistore_test
+                            ntfy.sh/resistore
                     """
         os.system(curl_cmd)
     prev_result = result
